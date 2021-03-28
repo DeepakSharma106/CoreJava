e4cf071c76a56f6b7cb83e7eb2813b113d7bbdb9
Deepaksharma106 <deepak.salwan13@gmail.com>
2021-03-28 22:04:43 +0530

    adding recrusive testing

--- src/main/java/com/sample/HelloWorld.java
+++ src/main/java/com/sample/HelloWorld.java
@@ -4,17 +4,18 @@ package com.sample;
 public class HelloWorld {
     public static void main(String[] args) {
 
+/*
         System.out.println("Iterating normally");
         for (int i = 0; i < 10; i++) {
             System.out.print("house no "+i+" , ");
         }
         System.out.println();
-
-        System.out.println("doing the same thing recursively");
+       System.out.println("doing the same thing recursively");
         new HelloWorld().housing(1);
+*/
 
-
-
+        new HelloWorld().iteratively();
+        new HelloWorld().recursively(1);
     }
 
     public void housing(int i){
@@ -26,5 +27,23 @@ public class HelloWorld {
             housing(i + 1);
             System.out.println("ending housing for house no "+i);
         }
+    }
+
+
+    public void iteratively(){
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i+" ");
+        }
+    }
+
+    public void recursively(int i){
+        if(i == 10)
+            return;
+        else {
+            System.out.println("going to executing function "+i);
+            recursively(i+1);
+            System.out.println("done executing function "+i);
+        }
+
     }
 }
